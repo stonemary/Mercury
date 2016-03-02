cf74ba362774941b394a1efacb500a683fc9dd46
Shi  Hu <doe@Shis-MacBook-Pro.local>
2016-03-02 02:26:48 -0800

    added cookbook packaging

--- tools/package.sh
+++ tools/package.sh
@@ -5,6 +5,10 @@
 # - zip the archive for the layer
 # - uploads to s3
 
+## options
+# -l layer
+# -t type: cookbooks or code
+
 
 # A POSIX variable
 OPTIND=1         # Reset in case getopts has been used previously in the shell.
@@ -13,14 +17,17 @@ OPTIND=1         # Reset in case getopts has been used previously in the shell.
 layer="doubanMovie"
 verbose=0
 tmp="/tmp/"
+type="code"
 
-while getopts "h?vl:" opt; do
+while getopts "h?vt:l:" opt; do
     case "$opt" in
     h|\?)
         show_help
         exit 0
         ;;
     v)  verbose=1
+        ;;
+    t)  type=$OPTARG
         ;;
     l)  layer=$OPTARG
         ;;
@@ -35,19 +42,27 @@ echo "verbose=$verbose, layer='$layer', Leftovers: $@"
 
 timestamp=`date +%Y%m%d%m%s`
 
-temp_dir=${tmp}${layer}-${timestamp}
+temp_dir=${tmp}${type}-${layer}-${timestamp}
 
 # pack up from git
 git clone https://github.com/stonemary/Mercury.git $temp_dir
 cd $temp_dir
 
 # archive folder sepecific for layer
-if [[ "$layer" == "doubanMovie" ]]
+if [[ "$type" == "$code" ]]
   then
-    git archive --format zip HEAD $layer > ${temp_dir}.zip
+    if [[ "$layer" == "doubanMovie" ]]
+      then
+        git archive --format zip HEAD $layer > ${temp_dir}.zip
+    fi
+  else
+    if [[ "$layer" == "doubanMovie" ]]
+      then
+        git archive --format zip HEAD cookbooks/mercury-crawler > ${temp_dir}.zip
+    fi
 fi
 
 # upload to S3
-aws s3 cp ${temp_dir}.zip s3://software.mercury.com/code/
+aws s3 cp ${temp_dir}.zip s3://software.mercury.com/${type}/
 
 # End of file
